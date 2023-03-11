2601bac4c26df5b2130744875ca80fe48f42d00f
novarlic <n.novarlic@innopolis.ru>
2023-03-11 23:19:25 +0300

    Authorization and Pass order

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+from .views import AlumniDetailAPI, RegisterAlumniAPIView
+
+router = routers.DefaultRouter()
+router.register(r'alumni', views.AlumniViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path("accounts/profile/", AlumniDetailAPI.as_view()),
+    path('register', RegisterAlumniAPIView.as_view()),
+    path('pass/', views.request_pass, name='pass'),
+]
