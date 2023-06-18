4291ffbb16a35d3e67f7f9834c6a731c4b45b635
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-06-19 02:39:28 +0300

    Fix allowed hosts

--- Backend/alumni_portal/settings.py
+++ Backend/alumni_portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j!x*&4_(-r&v9f-xr-lp7iq=ve+ak-bqzb5z6*pr=ffzxnlegg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://127.0.0.1:3000']
 
 CSRF_COOKIE_SAMESITE = "Lax"
 SESSION_COOKIE_SAMESITE = "Lax"
