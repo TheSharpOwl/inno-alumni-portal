1ac066342a197b7ff5deed21b534097ceb298072
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-06-19 11:09:09 +0300

    fix django errors

--- Backend/alumni_portal/settings.py
+++ Backend/alumni_portal/settings.py
@@ -67,15 +67,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-}
-
 ROOT_URLCONF = 'alumni_portal.urls'
 
 TEMPLATES = [
