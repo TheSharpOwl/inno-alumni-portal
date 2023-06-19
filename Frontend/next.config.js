c35a79b31f880ad0709afb0562a518ba52511dc2
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-06-19 16:36:20 +0300

    return commented block

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -17,14 +17,13 @@ module.exports = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/:path*',
-      },
-    ]
-  },
+  }
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://alumni.pythonanywhere.com/:path*',
+  //     },
+  //   ]
+  // },
 }
-  
