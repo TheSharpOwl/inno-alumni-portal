c7d692d4d24ffba175834d69e314fc29b5b1925a
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-06-19 14:00:45 +0300

    x

--- Backend/alumni_portal/settings.py
+++ Backend/alumni_portal/settings.py
@@ -42,7 +42,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://ziadxkabakibi.fvds.ru",
-    "*"
+    "http://*"
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_HTTPONLY = True
@@ -58,7 +58,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://ziadxkabakibi.fvds.ru",
-    "*"
+    "http://*"
 ]
 
 # Application definition
