d2290cb275f3efd70f0b5a872608c756872385fc
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-06-19 13:48:39 +0300

    update settings

--- Backend/alumni_portal/settings.py
+++ Backend/alumni_portal/settings.py
@@ -25,19 +25,19 @@ SECRET_KEY = 'django-insecure-j!x*&4_(-r&v9f-xr-lp7iq=ve+ak-bqzb5z6*pr=ffzxnlegg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:3000', 'http://localhost:8000/', 'http://localhost', 'http://localhost:3000', 'http://127.0.0.1:8000', 'http://ziadxkabakibi.fvds.ru', 'localhost:8000', 'localhost:3000', 'localhost', '127.0.0.1:8000', '127.0.0.1:3000', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1'','188.120.255.101', 'ziadxkabakibi.fvds.ru'']
 
 CSRF_COOKIE_SAMESITE = "Lax"
 SESSION_COOKIE_SAMESITE = "Lax"
 # CORS_ALLOWED_ORIGINS = ['*']
  # set this
 CORS_ALLOW_ALL_ORIGINS= False
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:3000', 'http://localhost:8000', 'http://localhost', 'http://localhost:3000', 'http://127.0.0.1:8000' , 'http://ziadxkabakibi.fvds.ru']
+CORS_ALLOWED_ORIGINS = ['http://188.120.255.101:3000', 'http://188.120.255.101:8000', 'http://188.120.255.101', 'http://127.0.0.1:3000', 'http://localhost:8000', 'http://localhost', 'http://localhost:3000', 'http://127.0.0.1:8000' , 'http://ziadxkabakibi.fvds.ru']
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_HTTPONLY = True
 SESSION_COOKIE_HTTPONLY = True
 CORS_EXPOSE_HEADERS = ["Content-Type", "X-CSRFToken"]
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3000', 'http://localhost:8000', 'http://localhost', 'http://localhost:3000', 'http://127.0.0.1:8000', 'http://ziadxkabakibi.fvds.ru']
+CSRF_TRUSTED_ORIGINS = ['http://188.120.255.101:3000', 'http://188.120.255.101:8000', 'http://188.120.255.101', 'http://127.0.0.1:3000', 'http://localhost:8000', 'http://localhost', 'http://localhost:3000', 'http://127.0.0.1:8000', 'http://ziadxkabakibi.fvds.ru']
 
 # Application definition
 
