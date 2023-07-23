a6bef18bb675c0b5f85499bc531403b74f4150a7
Daniel Atonge <elamboatonge@gmail.com>
2023-07-23 06:29:19 +0300

    configures next.config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:9001/api/v1/:path*"
-            : "/api/v1/",
+            : "/app/",
       },
     ];
   },
