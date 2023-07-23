ac0c66cd84f4c0145a14c315de90f4b7a6e9c3b8
elamboatonge <elamboatonge@gmail.com>
2023-07-23 21:13:12 +0300

    config alumni project

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: "http://127.0.0.1:9001/api/v1/:path*"
-      }
-    ];
-  }
 };
