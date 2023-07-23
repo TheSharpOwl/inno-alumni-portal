671b00502b3af24163a683f0269d1235f619fefb
elamboatonge <elamboatonge@gmail.com>
2023-07-23 20:49:34 +0300

    config alumni project

--- next.config.js
+++ next.config.js
@@ -4,11 +4,8 @@ module.exports = {
     return [
       {
         source: "/api/v1/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:9001/api/v1/:path*" :
-          "/app/",
-      },
+        destination: "http://127.0.0.1:9001/api/v1/:path*"
+      }
     ];
-  },
+  }
 };
--- src/api/sendRequest.js
+++ src/api/sendRequest.js
@@ -1,37 +1,41 @@
-import 'isomorphic-unfetch';
-import notify from '../utils/notify';
+import "isomorphic-unfetch";
+import notify from "../utils/notify";
 
 let { PORT, ROOT_URL } = process.env || {};
 PORT = PORT || 9001;
-ROOT_URL = ROOT_URL || ""//`http://127.0.0.1:${PORT}`;
+ROOT_URL = ROOT_URL || `http://127.0.0.1:${PORT}`;
 
 const sendRequest = async (path, options = {}) => {
-    const removeContentType = options.headers && options.headers['Remove-Content-Type']
-    let headers;
-    if (removeContentType) {
-        headers = { ...(options.headers || {}), };
-        delete options.headers['Remove-Content-Type']
-    } else {
-        headers = { 'Content-Type': 'application/json; charset=UTF-8', ...(options.headers || {}), };
-    }
+  const removeContentType =
+    options.headers && options.headers["Remove-Content-Type"];
+  let headers;
+  if (removeContentType) {
+    headers = { ...(options.headers || {}) };
+    delete options.headers["Remove-Content-Type"];
+  } else {
+    headers = {
+      "Content-Type": "application/json; charset=UTF-8",
+      ...(options.headers || {})
+    };
+  }
 
-    const response = await fetch(`${ROOT_URL}${path}`, {
-        method: 'POST',
-        credentials: 'include',
-        ...options,
-        headers,
-    });
+  const response = await fetch(`${ROOT_URL}${path}`, {
+    method: "POST",
+    credentials: "include",
+    ...options,
+    headers
+  });
 
-    console.log(response);
-    if (!response.ok) {
-        const failedInformation = await response.json();
-        console.log(failedInformation);
-        notify({ notificationMessage: failedInformation.detail })
-        throw new Error(failedInformation.detail)
-    }
-    const jsonData = await response.json();
+  console.log(response);
+  if (!response.ok) {
+    const failedInformation = await response.json();
+    console.log(failedInformation);
+    notify({ notificationMessage: failedInformation.detail });
+    throw new Error(failedInformation.detail);
+  }
+  const jsonData = await response.json();
 
-    return jsonData;
+  return jsonData;
 };
 
 export default sendRequest;
